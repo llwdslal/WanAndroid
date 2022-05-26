0d9fcc475d3f84f4e137ccc4e80e11004117da79
llwdslal <466186888@qq.com>
2022-05-26 16:01:32 +0800

    添加 ui-home 模块， buildSrc 管理依赖

---
+++ buildSrc/src/main/java/com/rock/buildsrc/ProjectConfig.kt
@@ -0,0 +1,14 @@
+package com.rock.buildsrc
+
+object ProjectConfig {
+    const val compileSdkVersion = 32
+    const val minSdkVersion = 26
+    const val targetSdkVersion = 32
+    const val applicationId = "com.rock.wanandroid"
+    const val kotlinCompilerExtensionVersion = Version.compose
+    const val versionCode = 1
+    const val versionName = "1.0"
+    const val jvmTarget = "1.8"
+    const val compose = true //启用 compose
+    const val minifyEnabled = true // 启用 R8 编译器
+}

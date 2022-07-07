b266990459cff2c357cf16ec9e753b15493a0779
llwdslal <466186888@qq.com>
2022-07-07 14:38:00 +0800

    完成 ComposeVmState

--- lib_base/src/main/java/com/rock/lib_base/arch/Action.kt
+++ lib_base/src/main/java/com/rock/lib_base/arch/Action.kt
@@ -1,9 +1,10 @@
 package com.rock.lib_base.arch
 
-interface IAction
-interface DataAction
+interface IAction //UI 层向 State 发送的事件，默认为 UI+Data 事件
+interface DataAction //单独的 Data 事件，同时实现 IAction 和 DataAction
 interface UiAction
 
+//事件消费者， ViewModel 和 ComposeVmState 实现此接口来处理事件
 interface ActionConsumer<A:IAction>{
     fun onAction(action:A)
 }
